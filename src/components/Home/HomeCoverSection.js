615a8f95fbff15de690ad46f268456fc01870834
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2023-11-13 19:38:59 -0500

    edited home screen

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -7,13 +7,18 @@ const HomeCoverSection = ({ blogs }) => {
     const blog = sortedBlogs[0];
     return (
         <div className="w-full inline-block">
-            <article className="w-full flex flex-col items-start justify-end mx-10 relative h-[85vh]">
+            <article className="flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]">
+                <div
+                    className="absolute top-0 left-0 bottom-0 right-0 h-full
+            bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0
+            "
+                />
                 <Image
                     src={blog.image.filePath.replace("../public", "")}
                     blurDataURL={blog.image.blurhashDataUrl}
                     alt={blog.title}
                     fill
-                    className="w-full h-full object-center object-cover rounded- 3x1"
+                    className="w-full h-full object-center object-cover rounded-3xl -z-10 "
                 />
             </article>
         </div>
